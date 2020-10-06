a7d8c3e10af7faf359e994dc879c08f16e66f069
Yoel Benítez Fonseca <ybenitezf@gmail.com>
2020-10-06 11:46:56 -0400

    add ldap config

--- auth/config.py
+++ auth/config.py
@@ -10,6 +10,18 @@ class Config(object):
     SECRET_KEY = os.getenv('SECRET_KEY') or 'some-secret-of-my-own'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    
+    # ldap integration
+    LDAP_AUTH = True
+    LDAP_HOST = '192.168.2.2'
+    LDAP_BASE_DN = 'DC=adelante,DC=lan'
+    LDAP_USER_DN = 'CN=Users'
+    LDAP_GROUP_DN = 'CN=Groups'
+    LDAP_USER_RDN_ATTR = 'CN'
+    LDAP_USER_LOGIN_ATTR = 'samAccountname'
+    LDAP_BIND_USER_DN = 'CN=Read Only,CN=Users,DC=adelante,DC=lan'
+    LDAP_BIND_USER_PASSWORD = 'zp3N7qvu'
+    # --
 
 class TestConfig(Config):
     TESTING = True
