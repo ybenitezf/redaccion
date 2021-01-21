81813d0da88c2ff7dd4e2e507209d2365243235e
Yoel Benítez Fonseca <ybenitezf@gmail.com>
2021-01-21 15:31:54 -0500

    Clase base para la páginación con Whoosh
    ref #8

--- app/application/modules/search.py
+++ app/application/modules/search.py
@@ -1,6 +1,7 @@
-from flask import current_app
-from application import celery
+from application import celery, db
+from whoosh.searching import Hit, ResultsPage
 from whoosh.filedb.filestore import FileStorage
+from flask import current_app
 
 def index_document(indice: str, data: dict):
     store = FileStorage(indice)
@@ -12,3 +13,52 @@ def index_document(indice: str, data: dict):
 @celery.task
 def index_document_async(*args, **kwargs):
     index_document(*args, **kwargs)
+
+
+class PaginaBusqueda(object):
+
+    def __init__(self, results: ResultsPage):
+        self._res = results
+        self._objects = []
+        if self.is_empty() is False:
+            self._objects = self._getObjectsFromResults()
+        self.pagenum = self._res.pagenum
+
+    def next(self):       
+        return self.pagenum + 1 if self.has_next() else self.pagenum
+    
+    def prev(self):
+        return self.pagenum - 1  if self.has_prev() else self.pagenum
+
+    def is_empty(self):
+        return self._res.results.is_empty()
+
+    def groups(self, name=None):
+        return self._res.results.groups(name=name)
+
+    def has_next(self):
+        return self._res.is_last_page() is False
+
+    def has_prev(self):
+        return self.pagenum > 1
+
+    def getObjects(self):
+        return self._objects
+
+    def _getObjectFromResult(self, hit: Hit):
+        return self.getObjectModel().query.get(
+            hit.get(self.getObjectIdentifier()))
+
+    def _getObjectsFromResults(self):
+        return [self._getObjectFromResult(r) for r in self._res]
+
+    def getObjectIdentifier(self) -> 'str':
+        """Atributo en los resultados que identifica al objeto
+        
+        Para poder ser usando en getObjectsFromResults
+        """
+        raise NotImplementedError
+
+    def getObjectModel(self) -> db.Model:
+        """Retorna el modelo base"""
+        raise NotImplementedError
